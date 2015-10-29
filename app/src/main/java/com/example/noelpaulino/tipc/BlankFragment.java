3206b3fa007d898a798e187c4fbf074001d98e84
NOELandVoid <paulino_noel2002@yahoo.com>
2015-10-29 19:58:09 +0800

    Initial commit

---
+++ app/src/main/java/com/example/noelpaulino/tipc/BlankFragment.java
@@ -0,0 +1,44 @@
+package com.example.noelpaulino.tipc;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BlankFragment extends Fragment {
+    private long workoutId;
+    @Override
+    public void onStart() {
+        super.onStart();
+        View view = getView();
+        if (view != null) {
+            TextView title = (TextView) view.findViewById(R.id.textTitle);
+            Workout workout = Workout.workouts[(int) workoutId];
+            title.setText(workout.getName());
+            TextView description = (TextView) view.findViewById(R.id.textDescription);
+            description.setText(workout.getDescription());
+        }
+    }
+    public BlankFragment() {
+        // Required empty public constructor
+    }
+    public void setWorkout(long id) {
+        this.workoutId = id;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_blank, container, false);
+    }
+
+
+}
